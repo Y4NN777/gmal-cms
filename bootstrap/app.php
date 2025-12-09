d98f40a3ab6d7008704c99651ffcad602103b19d
Y4NN777 <axel.studiesmail@gmail.com>
2025-12-09 07:46:28 +0000

    Configure trust proxies for HTTPS behind Render proxy
    - Trust all proxies to properly detect HTTPS from X-Forwarded-Proto header
    - This fixes mixed content errors (HTTP assets on HTTPS page)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Trust all proxies for Render.com deployment
+        $middleware->trustProxies(at: '*', headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR | \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST | \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT | \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO | \Illuminate\Http\Request::HEADER_X_FORWARDED_AWS_ELB);
+        
         $middleware->web([
             \App\Http\Middleware\HandleInertiaRequests::class,
             \App\Http\Middleware\SetLocale::class,
